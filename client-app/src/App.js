8d5b300470983a7034da27f580e355d781867e19
Eu não ligo kk <izaiassaturninodelimaneto@hotmail.com>
2023-07-17 23:47:06 -0300

    quick layout fix

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -301,7 +301,7 @@ function App() {
   );
 
   return (
-    <div className="app">
+    <div className="app p-20">
       <div className="login-form">
         <form onSubmit={clearNavigation}>
           <div className="row w-100 pb-3">
