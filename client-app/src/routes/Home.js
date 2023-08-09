61569394680dd721e3e2fa4e76c4d007f24b86aa
Tesche2 <nikolastesche@gmail.com>
2023-08-08 22:35:32 -0300

    Added a search bar to the upper menu

--- client-app/src/routes/Home.js
+++ client-app/src/routes/Home.js
@@ -3,47 +3,19 @@ import { useNavigate, useLocation, Link } from 'react-router-dom';
 
 import Login from "./Login.js";
 import AccountForm from "../components/AccountForm";
-import UpperMenu from "./UpperMenu.js"
-
-const events = [
-    { title: "Event 1", content: "Event 1 description" },
-    { title: "Event 2", content: "Event 2 description" },
-    { title: "Event 3", content: "Event 3 description" },
-    { title: "Event 4", content: "Event 4 description" },
-    { title: "Event 5", content: "Event 5 description" },
-    { title: "Event 6", content: "Event 6 description" },
-    { title: "Event 7", content: "Event 7 description" },
-    { title: "Event 8", content: "Event 8 description" },
-    { title: "Event 9", content: "Event 9 description" },
-    { title: "Event 10", content: "Event 10 description" },
-]
+import UpperMenu from "../components/UpperMenu.js"
+import EventsList from "../components/EventsList.js";
 
 const Content = (props) => {
     return (
         <div>
-            {/*
-            <div>Usuário {props.state.username} logado com sucesso.</div>
-            <div>Usuário logado como {props.state.utype}.</div>
-            {!props.state.validatedKitchen && props.state.utype === "cozinha solidária" ? (
-                <div>Seu cadastro ainda não foi validado.</div>
-            ) : (
-                <div></div>
-            )}
-            {props.state.validatedKitchen && props.state.utype === "cozinha solidária" ? (
-                <div>Seu cadastro está validado.</div>
-            ) : (
-                <div></div>
-            )}
-            */}
             <UpperMenu/>
-            <div className="event-list-container">
-            {events.map((event, index) => (
-                <EventCard
-                key={index}
-                title={event.title}
-                content={event.content}
-                />
-            ))}
+            <EventsList/>
+
+            <div className="bar">
+                <div className="bar-a"/>
+                <div className="bar-b"/>
+                <div className="bar-c"/>
             </div>
         </div>
     )
@@ -73,14 +45,5 @@ const Home = (props) => {
         </div>
     )
 };
-
-const EventCard = ({ title, content }) => {
-    return (
-        <div className="event-card-container">
-            <div className="event-card-title">{title}</div>
-            <div className="event-card-content">{content}</div>
-        </div>
-    )
-}
 
 export default Home;
