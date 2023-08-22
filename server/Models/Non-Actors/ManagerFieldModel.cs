587989edd10c36f71fb44658d2dd4f93528c3e8f
Eu não ligo kk <izaiassaturninodelimaneto@hotmail.com>
2023-08-22 19:49:46 -0300

    Revert "back-end updated"
    This reverts commit b2e7f5f1b3817e263210c99442f0d7e95a688c22.

---
+++ server/Models/Non-Actors/ManagerFieldModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace INF_01127.Models.Use_Cases
+{
+    public class ManagerFieldModel
+    {
+        [Required(ErrorMessage = "Senha é necessária.")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$", ErrorMessage = "Senha precisa ter no mínimo oito caracteres, no máximo dez caracteres, uma letra maiúscula, uma letra minúscula e um caractere especial.")]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Column("ManagerEmailAddress")]
+        [Required(ErrorMessage = "Endereço de email de gerente é necessário.")]
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage = "Endereço de email precisa ser válido.")]
+        [Display(Name = "EmailAddress")]
+        public string ManagerEmailAddress { get; set; }
+
+        [Column("UserEmailAddress")]
+        [Required(ErrorMessage = "Endereço de email de usuário é necessário.")]
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage = "Endereço de email precisa ser válido.")]
+        [Display(Name = "EmailAddress")]
+        public string UserEmailAddress { get; set; }
+    }
+}
